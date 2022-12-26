59e195a9b69afd76092d27214d5f49c49c6769f8
Torpass <pastor.jimenez.0806@gmail.com>
2022-12-26 14:36:36 -0400

    Validaciones en ingreso de datos del personaje

--- project_final_attempt/Forms/gestion_personajes.cs
+++ project_final_attempt/Forms/gestion_personajes.cs
@@ -1,4 +1,5 @@
-﻿using project_final_attempt.Entities.Personajes;
+﻿using project_final_attempt.Entities.Peliculas;
+using project_final_attempt.Entities.Personajes;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -56,22 +57,31 @@ namespace project_final_attempt.Forms
 
         private void btnEnviar_Click(object sender, EventArgs e)
         {
-            Personaje aux = new Personaje();
 
-            aux._name = txtNombre.Text;
-            aux._age = Convert.ToInt16(txtEdad.Text);
-            aux._sex = txtSexo.Text;
-            aux._rol = txtActitud.Text;
-            aux._identity = txtIdentidad.Text;
-            aux._universe = txtUniverso.Text;
-            aux._img = $"{aux._name}.jpeg";
-            ruta_imagen += $"{aux._img}";
-            if (btnTrue.Checked == true) { aux._activity = true; } else { aux._activity = false; }
-            ImagenPersonaje.Image.Save(ruta_imagen, ImageFormat.Jpeg);
+            if (!encontrar(txtNombre.Text))
+            {
+                if (validar_datos())
+                {
+                    Personaje aux = new Personaje();
 
-            personajes.heroe_create(aux);
-            personajes.serealizar_personaje();
-            MessageBox.Show("Datos almacenados en el Archivo", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    aux._name = txtNombre.Text;
+                    aux._age = Convert.ToInt16(txtEdad.Text);
+                    aux._sex = txtSexo.Text;
+                    aux._rol = txtActitud.Text;
+                    aux._identity = txtIdentidad.Text;
+                    aux._universe = txtUniverso.Text;
+                    aux._img = $"{aux._name}.jpeg";
+                    ruta_imagen += $"{aux._img}";
+                    if (btnTrue.Checked == true) { aux._activity = true; } else { aux._activity = false; }
+                    ImagenPersonaje.Image.Save(ruta_imagen, ImageFormat.Jpeg);
+
+                    personajes.heroe_create(aux);
+                    personajes.serealizar_personaje();
+                    MessageBox.Show("Datos almacenados en el Archivo", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }else { MessageBox.Show("No pueden haber datos vacios, intente nuevamente"); }
+            }
+            else { MessageBox.Show("No puedes ingresar el mismo personajes dos veces"); } //mejorar
+            
         }
 
 
@@ -92,8 +102,33 @@ namespace project_final_attempt.Forms
                 if (persoanje_encontrado._activity) { btnTrue.Checked = true; } else { btnFalse.Checked = true;  }
             }else 
             { 
-                MessageBox.Show("No se encontró ni pinga"); 
+                MessageBox.Show("No se encontró ni pinga");  //mejorar
             }
+        }
+
+        private bool encontrar(string nombre_persoanje)
+        {
+            foreach (Personaje aux in personajes.personajes_desceralizados())
+            {
+                if (aux._name == nombre_persoanje)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool validar_datos()
+        {
+            if (txtNombre.Text == "") { return false; }
+            if ( txtEdad.Value <= 0) { return false; }
+            if ( txtIdentidad.Text == "") { return false; }
+            if ( txtSexo.Text == "") { return false; }
+            if ( txtUniverso.Text  == "") { return false; }
+            if ( txtActitud.Text == "") { return false; }
+            if ( ImagenPersonaje.Image == null) { return false; }
+
+            else { return true; }
         }
     }
 }
